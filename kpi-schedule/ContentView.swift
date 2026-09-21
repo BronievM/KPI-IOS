c15f13e63a1c4f291b1bdb8baaf2dc52cdb0a7db
Maksym Broniev <bronievcorp@gmail.com>
2026-09-21 10:19:26 +0300

    Initial Commit

---
+++ kpi-schedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kpi-schedule
+//
+//  Created by Maksym on 21.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kpi-schedule/kpi_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  kpi_scheduleApp.swift
+//  kpi-schedule
+//
+//  Created by Maksym on 21.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct kpi_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
